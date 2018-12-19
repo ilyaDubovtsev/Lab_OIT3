0b7eba66b27c8c7867c1779dd7ac83b04e5c6f38
ilyaDubovtsev <dubovtsev98@gmail.com>
2018-12-19 21:30:59 +0400

    assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(window).width() > 1024){
+            if($(this).scrollTop() >= ($(window).height() / 3)) {
+                $('.toTop').fadeIn();
+            }
+            else {
+                $('.toTop').fadeOut();
+            }
+        }
+        else{
+            $('.toTop').hide();
+        }
+    });
+    $('.toTop').click(function() {
+        $('body,html').animate({scrollTop:0},800);
+
+    });
+});
+
+function TileClose(id) {
+    $("#"+id).hide();
+}
+
+function TileOpen(id) {
+    $("#"+id).show();
+}
